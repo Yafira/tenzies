5c7375480e02da27dd8224d718306d7e879d3bd0
Yafira <yafira.martinez@lc.cuny.edu>
2022-04-04 23:45:45 -0400

    generated array of 10 rand numbers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import Die from './components/Die'
 import './style.css'
 
 export default function App() {
+	function allNewDice() {
+		// new array to hold the numbers
+		const newDice = []
+		// loop 10 times
+		for (let i = 0; i < 10; i++) {
+			// push a random number from 1-6 to my array
+			newDice.push(Math.ceil(Math.random() * 6))
+		}
+		// return array
+		return newDice
+	}
+	console.log(allNewDice())
+
 	return (
 		<main>
 			<div className='dice-container'>
