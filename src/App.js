075c3032f22dd150aca251239c0021675260150f
Yafira <yafira.martinez@lc.cuny.edu>
2022-04-04 23:40:29 -0400

    implemented die components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import Die from './components/Die'
 import './style.css'
-import './App.css'
 
 export default function App() {
-	return <main></main>
+	return (
+		<main>
+			<div className='dice-container'>
+				<Die value='1' />
+				<Die value='2' />
+				<Die value='3' />
+				<Die value='4' />
+				<Die value='5' />
+				<Die value='6' />
+				<Die value='7' />
+				<Die value='8' />
+				<Die value='9' />
+				<Die value='0' />
+			</div>
+		</main>
+	)
 }
