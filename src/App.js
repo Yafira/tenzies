e4fdbb8fe8f3e33fed380c40504434e034270405
Yafira <yafira.martinez@lc.cuny.edu>
2022-04-06 20:32:09 -0400

    implemented holdDice functionality

--- src/App.js
+++ src/App.js
@@ -22,8 +22,17 @@ export default function App() {
 		setDice(allNewDice())
 	}
 
+	function holdDice(id) {
+		console.log(id)
+	}
+
 	const diceElements = dice.map((die) => (
-		<Die key={die.id} value={die.value} isHeld={die.isHeld} />
+		<Die
+			key={die.id}
+			value={die.value}
+			isHeld={die.isHeld}
+			holdDice={() => holdDice(die.id)}
+		/>
 	))
 
 	return (
